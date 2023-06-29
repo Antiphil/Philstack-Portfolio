e55180204d3b9e17ad5ed927adeccf2cb14075d0
Phil <92030086+MrNamazu@users.noreply.github.com>
2023-06-29 23:03:26 +0200

    Update nav.svelte

--- src/lib/components/layout/nav.svelte
+++ src/lib/components/layout/nav.svelte
@@ -6,21 +6,13 @@
 {#if $navOpen}
 	<div class="flex justify-center">
 		<div in:fly={{ x: -2000, duration: 1000 }} out:fly={{ x: -2000, duration: 1000 }} class="fixed bg w-screen h-screen z-40" />
-		<div in:fly={{ x: -2000, duration: 1500 }} out:fly={{ x: -2000, duration: 1000 }} class="fixed bg-[#0a0a0c] w-[calc(100%-256px)] h-screen z-40" />
+		<div in:fly={{ x: -2000, duration: 1500 }} out:fly={{ x: -2000, duration: 1000 }} class="fixed bg-[#0a0a0c] w-[95%] lg:w-[calc(100%-256px)] h-screen z-40" />
 	</div>
 {/if}
 
-
 <style>
 	.bg {
-		background-color:#9999ff;
-		background-image:
-		radial-gradient(at 49% 99%, hsla(278,71%,74%,1) 0px, transparent 50%),
-		radial-gradient(at 56% 74%, hsla(95,77%,69%,1) 0px, transparent 50%),
-		radial-gradient(at 97% 73%, hsla(162,71%,67%,1) 0px, transparent 50%),
-		radial-gradient(at 94% 64%, hsla(200,62%,78%,1) 0px, transparent 50%),
-		radial-gradient(at 37% 94%, hsla(191,66%,61%,1) 0px, transparent 50%),
-		radial-gradient(at 72% 64%, hsla(188,70%,61%,1) 0px, transparent 50%),
-		radial-gradient(at 3% 10%, hsla(353,89%,77%,1) 0px, transparent 50%);
+		background-color: #9999ff;
+		background-image: radial-gradient(at 49% 99%, hsla(278, 71%, 74%, 1) 0px, transparent 50%), radial-gradient(at 56% 74%, hsla(95, 77%, 69%, 1) 0px, transparent 50%), radial-gradient(at 97% 73%, hsla(162, 71%, 67%, 1) 0px, transparent 50%), radial-gradient(at 94% 64%, hsla(200, 62%, 78%, 1) 0px, transparent 50%), radial-gradient(at 37% 94%, hsla(191, 66%, 61%, 1) 0px, transparent 50%), radial-gradient(at 72% 64%, hsla(188, 70%, 61%, 1) 0px, transparent 50%), radial-gradient(at 3% 10%, hsla(353, 89%, 77%, 1) 0px, transparent 50%);
 	}
 </style>
