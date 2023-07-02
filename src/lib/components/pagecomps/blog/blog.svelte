d57c6f854500f89a462d2b12b427c1b887621c00
Phil <92030086+MrNamazu@users.noreply.github.com>
2023-07-02 19:56:23 +0200

    .

--- src/lib/components/pagecomps/blog/blog.svelte
+++ src/lib/components/pagecomps/blog/blog.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
 	import Title from '$lib/components/modules/title.svelte';
+	import { formatDate } from '$lib/utils';
 	import Blogcard from './blogcard.svelte';
 
 	export let data: any;
 </script>
 
-<section class="py-6 sm:py-32">
+<!-- <section class="py-6 sm:py-32">
 	<div class="container p-6 mx-auto space-y-8">
 		<Title title1="Meine neusten und besten Posts" title2="Mein Portfolio, Mein Blog" />
 
@@ -16,3 +17,44 @@
 		</div>
 	</div>
 </section>
+ -->
+
+<section class="py-6 sm:py-32">
+	<Title title1="Meine neusten und besten Posts" title2="Mein Portfolio, Mein Blog" />
+	<div class="mx-5 lg:mx-32">
+		<div class="bg-black/30 flex p-3">
+			<div class="w-1/3 flex flex-col">
+				{#each data.posts as post, i}
+					<div class="flex gap-3 group-[blog] hover:bg-black/30 p-3 transition">
+						<div class="relative">
+							<span class="absolute test w-12 shadow-red-700 ml-2">{'0' + (1 + i)}</span>
+							<img class="min-w-[80px] w-20 h-20 opacity-25" src={`/${post.slug}.png`} alt="" />
+						</div>
+						<div class="relative flex flex-col w-[83%]">
+							<h2 class="font-bold uppercase">{post.title}</h2>
+							<p class="font-light text-xs truncate">{post.description}</p>
+							<div class="absolute bottom-0 w-full flex justify-between text-xs font-semibold">
+								<p>{formatDate(post.date)}</p>
+								<p>Phil</p>
+							</div>
+						</div>
+					</div>
+				{/each}
+			</div>
+			<div class="">
+				<span class="test"> Laser Cutting - Meine Einstieg & Erfahrungen</span>
+			</div>
+		</div>
+	</div>
+</section>
+
+<style>
+	.test {
+		font-weight: 900;
+		font-size: 30px;
+		color: #fff;
+		-webkit-text-stroke-color: #fff;
+		-webkit-text-fill-color: transparent;
+		-webkit-text-stroke-width: 1px;
+	}
+</style>
---
+++ src/posts/social-media copy.md
@@ -0,0 +1,25 @@
+---
+title: Social Media
+description: Erfahre wie ich meine ersten Schritte im bereich der Laser Gravur & Cuttin gemacht habe und was dabei gelernt habe.
+date: '2023-6-27'
+categories:
+  - DeepDive
+  - LaserCutting
+  - Erfahrungsbericht
+  - Tipps & Tricks
+published: true
+---
+
+## Vorgeschichte - Wie kam ich dazu?
+
+Nachdem ich in der KreativWerkstatt in Meiningen zu arbeiten begonnen hatte und den dort bereits vorhandenen 3D-Drucker ausprobiert hatte, sprach meine Chefin eines Morgens mit mir über das Projekt Laser Cutting. Sie wollte gerne, dass wir unseren Kunden eine noch bessere Personalisierungsmöglichkeit anbieten können. Da das Thema Finanzierung bei der Lebenshilfe immer etwas mühsam ist, dauerte es eine Weile, bis die endgültige Anschaffung erfolgte. Zu diesem Zeitpunkt war es mir nicht ganz recht, mich im Voraus bereits mit dem Großteil des Wissens anzueignen, da das letzte Wort darüber, ob das Ganze überhaupt zustande kommt, noch nicht gesprochen war.
+
+Mitte März hat es dann doch endlich geklappt und plötzlich lag ein Paket von Amazon vor mir auf dem Tisch mit dem Sculpfun S9. Es ist kein besonders leistungsstarker Laser, aber für die ersten Schritte und den Hobbygebrauch ist er auf jeden Fall sein Geld wert!
+
+## Montage des Laser Cutters
+
+Die Montage ging ziemlich schnell vonstatten und dank der Anleitung, die vergleichbar mit der eines einfachen IKEA-Regals ist, war sie auch für die meisten Menschen einfach und übersichtlich. In der Aufbauanleitung gab es am Ende auch gleich die ersten Empfehlungen für verwendbare Software.
+
+## Erste Schritte
+
+Da die ersten Schritte mit dem Laser etwas überwältigend sein können und man oft nicht genau weiß, wo und wie man anfangen soll, hier eine kurze Übersicht...
