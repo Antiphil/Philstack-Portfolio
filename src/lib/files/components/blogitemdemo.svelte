de34ce8bf9d7f66f1bccc3a7fc16a649be9bb5a2
Phil <92030086+MrNamazu@users.noreply.github.com>
2024-03-19 17:07:28 +0100

    .

---
+++ src/lib/files/components/blogitemdemo.svelte
@@ -0,0 +1,70 @@
+<script>
+	import Button from '$lib/files/modules/button.svelte';
+</script>
+
+<section class="relative flex w-full py-[35px] pl-[70px]">
+	<div class="curved-corner-bottomright !absolute right-0 top-[calc(0px-65px)] !z-50"></div>
+	<div class="curved-corner-topleft !absolute left-0 top-[calc(0px-35px)] !z-50"></div>
+	<div class="relative flex h-full w-full flex-col items-center gap-3 overflow-hidden rounded-l-[75px] bg-secondary p-10">
+        <div class="flex flex-col gap-20 w-full items-center justify-center bg-secondary p-20">
+            <div class="relative flex h-[200px] w-full rounded-3xl border-2 border-[#EDE9D0] shadow-[4px_4px_0px_rgba(237,233,208,1)]">
+                <img class="relative z-30 w-1/3 rounded-3xl object-cover" src="https://cdn.leonardo.ai/users/372c505e-b23b-464e-80af-b4294fdd9866/generations/912a5f12-c946-4c7e-b080-266a13edaec0/Default_a_sepia_colored_comic_image_of_a_laser_cutter_1.jpg" alt="" />
+
+                <div class="right-10 relative flex justify-center items-center w-[90%]">
+                    <div class="absolute z-40  w-[400px] left-[-100px] top-[-10%] bg-[#EDE9D0] rounded-3xl text-secondary p-5">
+                        <h3 class="font-extrabold uppercase">Post about programming</h3>
+                        <h2 class="text-2xl font-extrabold uppercase">Laser Cutting: Meine Erfahrungen und mein Einstieg</h2>
+                        
+                    </div>
+                    <div class="absolute bottom-4 rounded-3xl w-36 h-6 bg-primary left-[160px]"></div>
+                    <div class="relative top-2 flex flex-col justify-center items-end">
+                        <div class="line-clamp-4 border-2 border-[#EDE9D0] w-full text-[#EDE9D0] rounded-xl pl-[26%] pr-1 py-1 text-sm italic">
+                        Dieser Blogbeitrag dokumentiert meine Reise durch Three.js und seine Anwendung in der Webentwicklung. Der Beitrag ist eine persönliche Anleitung, um das Wissen zu vertiefen, und beschreibt auch die Verwendung von React bzw. Next.js für die Portfolioerstellung.
+                        Dieser Blogbeitrag dokumentiert meine Reise durch Three.js und seine Anwendung in der Webentwicklung. Der Beitrag ist eine persönliche Anleitung, um das Wissen zu vertiefen, und beschreibt auch die Verwendung von React bzw. Next.js für die Portfolioerstellung.
+                        </div>
+                        <Button text="Read post"/>
+                    </div>
+                </div>
+            </div>
+            <div class="relative flex h-[200px] w-full rounded-3xl border-2 border-[#EDE9D0] shadow-[4px_4px_0px_rgba(237,233,208,1)]">
+                <img class="relative z-30 w-1/3 rounded-3xl object-cover" src="https://cdn.leonardo.ai/users/372c505e-b23b-464e-80af-b4294fdd9866/generations/912a5f12-c946-4c7e-b080-266a13edaec0/Default_a_sepia_colored_comic_image_of_a_laser_cutter_1.jpg" alt="" />
+
+                <div class="right-10 relative flex justify-center items-center w-[90%]">
+                    <div class="absolute z-40  w-[400px] left-[-100px] top-[-10%] bg-[#EDE9D0] rounded-3xl text-secondary p-5">
+                        <h3 class="font-extrabold uppercase">Post about programming</h3>
+                        <h2 class="text-2xl font-extrabold uppercase">Laser Cutting: Meine Erfahrungen und mein Einstieg</h2>
+                        
+                    </div>
+                    <div class="absolute bottom-4 rounded-3xl w-36 h-6 bg-primary left-[160px]"></div>
+                    <div class="relative top-2 flex flex-col justify-center items-end">
+                        <div class="line-clamp-4 border-2 border-[#EDE9D0] w-full text-[#EDE9D0] rounded-xl pl-[26%] pr-1 py-1 text-sm italic">
+                        Dieser Blogbeitrag dokumentiert meine Reise durch Three.js und seine Anwendung in der Webentwicklung. Der Beitrag ist eine persönliche Anleitung, um das Wissen zu vertiefen, und beschreibt auch die Verwendung von React bzw. Next.js für die Portfolioerstellung.
+                        Dieser Blogbeitrag dokumentiert meine Reise durch Three.js und seine Anwendung in der Webentwicklung. Der Beitrag ist eine persönliche Anleitung, um das Wissen zu vertiefen, und beschreibt auch die Verwendung von React bzw. Next.js für die Portfolioerstellung.
+                        </div>
+                        <Button text="Read post"/>
+                    </div>
+                </div>
+            </div>
+            <div class="relative flex h-[200px] w-full rounded-3xl border-2 border-[#EDE9D0] shadow-[4px_4px_0px_rgba(237,233,208,1)]">
+                <img class="relative z-30 w-1/3 rounded-3xl object-cover" src="https://cdn.leonardo.ai/users/372c505e-b23b-464e-80af-b4294fdd9866/generations/912a5f12-c946-4c7e-b080-266a13edaec0/Default_a_sepia_colored_comic_image_of_a_laser_cutter_1.jpg" alt="" />
+
+                <div class="right-10 relative flex justify-center items-center w-[90%]">
+                    <div class="absolute z-40  w-[400px] left-[-100px] top-[-10%] bg-[#EDE9D0] rounded-3xl text-secondary p-5">
+                        <h3 class="font-extrabold uppercase">Post about programming</h3>
+                        <h2 class="text-2xl font-extrabold uppercase">Laser Cutting: Meine Erfahrungen und mein Einstieg</h2>
+                        
+                    </div>
+                    <div class="absolute bottom-4 rounded-3xl w-36 h-6 bg-primary left-[160px]"></div>
+                    <div class="relative top-2 flex flex-col justify-center items-end">
+                        <div class="line-clamp-4 border-2 border-[#EDE9D0] w-full text-[#EDE9D0] rounded-xl pl-[26%] pr-1 py-1 text-sm italic">
+                        Dieser Blogbeitrag dokumentiert meine Reise durch Three.js und seine Anwendung in der Webentwicklung. Der Beitrag ist eine persönliche Anleitung, um das Wissen zu vertiefen, und beschreibt auch die Verwendung von React bzw. Next.js für die Portfolioerstellung.
+                        Dieser Blogbeitrag dokumentiert meine Reise durch Three.js und seine Anwendung in der Webentwicklung. Der Beitrag ist eine persönliche Anleitung, um das Wissen zu vertiefen, und beschreibt auch die Verwendung von React bzw. Next.js für die Portfolioerstellung.
+                        </div>
+                        <Button text="Read post"/>
+                    </div>
+                </div>
+            </div>
+          </div>
+          
+	</div>
+</section>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
-	import About from '$lib/files/components/home/about.svelte';
+	import Blogitemdemo from '$lib/files/components/blogitemdemo.svelte';
+import About from '$lib/files/components/home/about.svelte';
 	import Blog from '$lib/files/components/home/blog/blog.svelte';
 	import Hero from '$lib/files/components/home/hero.svelte';
 	import Projects from '$lib/files/components/home/projects/projects.svelte';
@@ -10,5 +11,5 @@
 	<About />
 	<Projects />
 	<Blog />
-	<Projects />
+	<Blogitemdemo />
 </div>
