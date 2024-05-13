099a6dbcc57b2f54c3a225b481e048bc61769fff
Phil <92030086+Antiphil@users.noreply.github.com>
2024-05-13 22:12:42 +0200

    .

--- src/lib/files/components/home/blog/blog.svelte
+++ src/lib/files/components/home/blog/blog.svelte
@@ -2,15 +2,16 @@
 	import Button from '$lib/files/modules/button.svelte';
 	import Title from '$lib/files/modules/title.svelte';
 	import { blogStore } from '$lib/stores/blog';
-	import Blogitem from './blogitem.svelte';
+	import Blogitem1 from './blogitem1.svelte';
+	import Blogitem2 from './blogitem2.svelte';
 	import { format, formatDistance, formatRelative, subDays } from 'date-fns';
 </script>
 
 <section class="w-full">
 	<div class="container mx-auto px-6 py-10">
-		<div class="mb-10 mt-6 md:flex md:items-center md:justify-between">
+		<div class="mb-10 max-w-screen-xl m-auto mt-6 md:flex md:items-center md:justify-between">
 			<div>
-				<h1 class="text-2xl font-bold uppercase lg:text-3xl">My Blog</h1>
+				<h1 class="text-2xl font-bold uppercase lg:text-3xl">Latest Articles</h1>
 
 				<div class="mx-auto mt-3 flex">
 					<span class="inline-block h-1 w-40 rounded-full bg-primary"></span>
@@ -20,29 +21,21 @@
 			</div>
 		</div>
 
-		<div class="flex w-full gap-5">
+		<div class="flex flex-col  w-full gap-5">
 			{#if $blogStore.data}
 				{#each $blogStore.data.reverse() as blog, index}
 					{#if index < 4}
-						<article class="group h-[600px] w-[25%] transform cursor-pointer overflow-hidden rounded-2xl bg-cover bg-center shadow-xl sepia duration-500 hover:-translate-y-2" style={`background-image: url(https://strapi.antiphil.de${blog?.attributes.media.data[0].attributes.url})`}>
-							<div class="relative flex h-[600px] transform flex-col bg-black bg-opacity-20 px-6 py-6 duration-300 hover:bg-opacity-75">
-								<h1 class="absolute bottom-10 mb-5 transform text-3xl font-medium drop-shadow-[1px_1px_0px_rgba(0,0,0,1)] duration-300 group-hover:translate-y-[-100px]">{blog.attributes.title}</h1>
-								<div class="absolute bottom-5 mb-5 flex h-2 w-full transform rounded-full drop-shadow-[1px_1px_0px_rgba(0,0,0,1)] duration-300 group-hover:translate-y-[-100px]">
-									<span class="inline-block h-1.5 w-20 rounded-full bg-primary"></span>
-									<span class="mx-1 inline-block h-1.5 w-6 rounded-full bg-primary"></span>
-									<span class="inline-block h-1.5 w-2 rounded-full bg-primary"></span>
-								</div>
-								<p class="absolute bottom-5 line-clamp-3 w-[calc(100%-48px)] translate-y-[-40px] transform text-sm opacity-0 drop-shadow-[1px_1px_0px_rgba(0,0,0,1)] duration-500 group-hover:opacity-80">Lorem ipsum dolor sit, amet consectetur adipisicing consectetur adipisicing consectetur adipisicing elit. Vitae, sapiente tempore iusto consectetur quo soluta consequatur. Ad officiis ea voluptas!</p>
-								<p class="absolute bottom-5 flex w-[calc(100%-48px)] translate-y-[-0px] transform justify-between pb-3 text-xs opacity-0 drop-shadow-[1px_1px_0px_rgba(0,0,0,1)] duration-500 group-hover:opacity-80">
-									<span>{formatDistance(subDays(blog.attributes.date, 3), new Date(), { addSuffix: true })}</span>
-									<span>Author: Phil</span>
-								</p>
-							</div>
-						</article>
+						{#if index % 2 == 0}
+							<Blogitem1 title={blog.attributes.title} desc={undefined} img={`https://strapi.antiphil.de${blog?.attributes.media.data[0].attributes.url}`} date={formatDistance(subDays(blog.attributes.date, 3), new Date(), { addSuffix: true })}/>
+						{:else}
+							<Blogitem2 title={blog.attributes.title} desc={undefined} img={`https://strapi.antiphil.de${blog?.attributes.media.data[0].attributes.url}`} date={formatDistance(subDays(blog.attributes.date, 3), new Date(), { addSuffix: true })}/>
+						{/if}
 					{/if}
 				{/each}
 			{/if}
 		</div>
-		<Button text="View Blog" link="/" />
+		<div class="flex justify-center mt-10">
+		<Button text="Visit blog page" link="/" />
+		</div>
 	</div>
 </section>
---
+++ src/lib/files/components/home/blog/blogitem1.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import phil from '$lib/assets/phil.jpg';
+	import Button from '$lib/files/modules/button.svelte';
+	import { text } from '@sveltejs/kit';
+	import { format, formatDistance, endOfDay, formatRelative, subDays } from 'date-fns';
+
+	export let title: string = 'Viel zu unzufrieden mit diesem ganzen Mist hier! ARGH!!!';
+	export let img: string = 'https://img.freepik.com/free-vector/cute-astronaut-friend-playing-space-cartoon-vector-icon-illustration-science-technology-isolated_138676-8720.jpg';
+	export let date: string = '0 seconds ago';
+	export let desc: string = 'Lorem ipsum dolor sit amet consectetur adip Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum natus, quia incidunt perferendis magnam rerum pariatur quidem?  Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum natus, quia incidunt perferendis magnam rerum pariatur quidem? isicing elit. Eum natus, quia incidunt perferendis magnam rerum pariatur quidem? Maxime magnam, ullam accusamus veniam veritatis voluptas, blanditiis, molestias nemo ipsam asperiores provident?';
+	export let url: string = "/"
+</script>
+
+
+<a href={url} class=" max-w-screen-xl m-auto relative hover:bg-secondary  hover:scale-[1.01] bg-primary group w-full p-5 h-72 rounded-2xl items-center justify-between flex border-[4px] border-primary transition-all">
+	<div class="border-[5px] border-secondary absolute top-[28px] -left-[32px] w-64 h-56 bg-primaryhover rounded-3xl overflow-hidden">
+		<img src={img} alt="" class="sepia w-full h-full object-cover" />
+	</div>
+	<div class="text-secondary group-hover:text-primary ml-[calc(224px-16px)] max-h-56 p-8">
+		<h3 class="text-xs font-semibold line-clamp-1">Posted {date} - Author: Phil</h3>
+		<h2 class="text-3xl font-bold mb-3 line-clamp-1">{title}</h2>
+		<h3 class="text-sm font-light line-clamp-3 text-justify">{desc}</h3>
+		<div class="mt-3 w-fit items-center gap-2 rounded-xl text-sm font-bold  transition-all group-hover:text-primary text-secondary flex">
+			<span>Read More</span>
+			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-6 w-6">
+				<path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
+			</svg>
+		</div>
+	</div>
+	<div class="flex flex-col gap-2">
+		<div class="rounded-xl h-6 w-4 group-hover:bg-primary bg-secondary ml-10"></div>
+		<div class="rounded-xl h-16 w-4 group-hover:bg-primary bg-secondary ml-10"></div>
+	</div>
+</a>
