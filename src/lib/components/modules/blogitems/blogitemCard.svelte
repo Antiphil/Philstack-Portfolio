36a49180f7731d6118e3b8746bedf2e17f041277
Phil <92030086+Antiphil@users.noreply.github.com>
2024-05-26 14:23:28 +0200

    .

---
+++ src/lib/components/modules/blogitems/blogitemCard.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	export let title: string = 'Viel zu unzufrieden mit diesem ganzen Mist hier! ARGH!!!';
+	export let img: string = 'https://img.freepik.com/free-vector/cute-astronaut-friend-playing-space-cartoon-vector-icon-illustration-science-technology-isolated_138676-8720.jpg';
+	export let date: string = '0 seconds ago';
+	export let desc: string = 'Lorem ipsum dolor sit amet consectetur adip Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum natus, quia incidunt perferendis magnam rerum pariatur quidem?  Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum natus, quia incidunt perferendis magnam rerum pariatur quidem? isicing elit. Eum natus, quia incidunt perferendis magnam rerum pariatur quidem? Maxime magnam, ullam accusamus veniam veritatis voluptas, blanditiis, molestias nemo ipsam asperiores provident?';
+	export let url: string = '/';
+</script>
+
+<a href={url} class="group relative m-auto mt-[60px] flex w-full max-w-screen-xl flex-col items-center justify-between rounded-2xl border-[4px] border-primary-main bg-primary-main p-5 transition-all hover:scale-[1.01] hover:bg-secondary-800">
+	<div class="absolute top-[-60px] h-56 w-64 overflow-hidden rounded-3xl border-[5px] border-secondary-800">
+		<img src={img} alt="" class="h-full w-full object-cover sepia" />
+	</div>
+	<div class="mt-36 p-2 text-secondary-800 group-hover:text-primary-main">
+		<h3 class="mb-2 line-clamp-1 text-xs font-semibold">Posted {date} - Author: Phil</h3>
+		<h2 class="mb-3 line-clamp-1 text-xl font-bold">{title}</h2>
+		<h3 class="line-clamp-3 text-justify text-sm font-light">{desc}</h3>
+		<div class="text-secondary group-hover:text-primary mt-3 flex w-fit items-center gap-2 rounded-xl text-sm font-bold transition-all">
+			<span>Read More</span>
+			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-6 w-6">
+				<path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
+			</svg>
+		</div>
+	</div>
+	<div class="hidden gap-2">
+		<div class="group-hover:bg-primary h-4 w-6 rounded-xl bg-secondary-800"></div>
+		<div class="group-hover:bg-primary h-4 w-16 rounded-xl bg-secondary-800"></div>
+	</div>
+</a>
--- src/routes/[lang=lang]/blog/+page.svelte
+++ src/routes/[lang=lang]/blog/+page.svelte
@@ -1,16 +1,21 @@
 <script>
+	import BlogitemCard from '$lib/components/modules/blogitems/blogitemCard.svelte';
 	import BlogitemMain from '$lib/components/modules/blogitems/blogitemMain.svelte';
 	import BlogitemSmall from '$lib/components/modules/blogitems/blogitemSmall.svelte';
 	import Button from '$lib/components/modules/button.svelte';
+	import Title from '$lib/components/modules/title.svelte';
 </script>
 
-<main class="m-auto mt-12 max-w-7xl">
+<main class="m-auto mt-12 max-w-7xl pb-20">
+	<div class="mb-8">
+		<Title left title="Featured Posts" />
+	</div>
 	<!-- FEATURED POSTS -->
 	<div class="mb-16 flex flex-wrap space-x-0 md:flex-nowrap md:space-x-6">
-		<div class="md:w-4/7 relative mb-3 block w-full rounded-3xl bg-primary-main p-3 text-secondary-800 lg:mb-0">
+		<div class="md:w-4/7 relative mb-3 block w-full pt-20 lg:mb-0">
 			<BlogitemMain />
 		</div>
-		<div class="md:w-4/7 xs:pl-7 flex w-full flex-col gap-3">
+		<div class="md:w-4/7 xs:pl-3 flex w-full flex-col gap-3">
 			<BlogitemSmall />
 			<BlogitemSmall />
 			<BlogitemSmall />
@@ -20,43 +25,19 @@
 
 	<!-- recent posts -->
 	<div class="mb-4 mt-16 flex items-center justify-between px-4 lg:px-0">
-		<h2 class="text-3xl font-bold">Latest news</h2>
-		<a class="cursor-pointer rounded bg-gray-200 px-3 py-1 text-gray-800 hover:bg-green-200"> View all </a>
+		<div class="">
+			<Title left title="Latest Posts" />
+		</div>
+		<Button text="View All" link="/" />
 	</div>
 	<div class="block space-x-0 lg:flex lg:space-x-6">
-		<div class="w-full rounded p-4 lg:w-1/2 lg:w-1/3 lg:p-0">
-			<img src="https://images.unsplash.com/photo-1526666923127-b2970f64b422?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60" class="rounded" alt="technology" />
-			<div class="p-4 pl-0">
-				<h2 class="text-2xl font-bold text-gray-800">Put all speaking her delicate recurred possible.</h2>
-				<p class="mt-2 text-gray-700">Set indulgence inquietude discretion insensible bed why announcing. Middleton fat two satisfied additions. So continued he or commanded household smallness delivered. Door poor on do walk in half. Roof his head the what.</p>
-
-				<a href="#" class="ml-auto mt-2 inline-block rounded py-2 text-green-900"> Read more </a>
-			</div>
-		</div>
-
-		<div class="w-full rounded p-4 lg:w-1/2 lg:w-1/3 lg:p-0">
-			<img src="https://images.unsplash.com/photo-1504384764586-bb4cdc1707b0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60" class="rounded" alt="technology" />
-			<div class="p-4 pl-0">
-				<h2 class="text-2xl font-bold text-gray-800">Is at purse tried jokes china ready decay an.</h2>
-				<p class="mt-2 text-gray-700">Small its shy way had woody downs power. To denoting admitted speaking learning my exercise so in. Procured shutters mr it feelings. To or three offer house begin taken am at.</p>
-
-				<a href="#" class="ml-auto mt-2 inline-block rounded py-2 text-green-900"> Read more </a>
-			</div>
-		</div>
-
-		<div class="w-full rounded p-4 lg:w-1/2 lg:w-1/3 lg:p-0">
-			<img src="https://images.unsplash.com/photo-1483058712412-4245e9b90334?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80" class="rounded" alt="technology" />
-			<div class="p-4 pl-0">
-				<h2 class="text-2xl font-bold text-gray-800">As dissuade cheerful overcame so of friendly he indulged unpacked.</h2>
-				<p class="mt-2 text-gray-700">Alteration connection to so as collecting me. Difficult in delivered extensive at direction allowance. Alteration put use diminution can considered sentiments interested discretion.</p>
-
-				<a href="#" class="ml-auto mt-2 inline-block rounded py-2 text-green-900"> Read more </a>
-			</div>
-		</div>
+		<BlogitemCard />
+		<BlogitemCard />
+		<BlogitemCard />
 	</div>
 	<!-- end recent posts -->
 
-	<!-- subscribe -->
+	<!-- subscribe
 	<div class="mt-12 flex rounded md:shadow">
 		<img src="https://images.unsplash.com/photo-1579275542618-a1dfed5f54ba?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60" class="w-0 rounded-l object-cover md:w-1/4" />
 		<div class="px-4 py-2">
@@ -72,43 +53,19 @@
 			</form>
 		</div>
 	</div>
-	<!-- ens subscribe section -->
+	ens subscribe section -->
 
 	<!-- popular posts -->
 	<div class="mb-4 mt-16 flex items-center justify-between px-4 lg:px-0">
-		<h2 class="text-3xl font-bold">Popular news</h2>
-		<a class="cursor-pointer rounded bg-gray-200 px-3 py-1 text-gray-800 hover:bg-green-200"> View all </a>
+		<div class="">
+			<Title left title="Popular Posts" />
+		</div>
+		<Button text="View All" link="/" />
 	</div>
 	<div class="block space-x-0 lg:flex lg:space-x-6">
-		<div class="w-full rounded p-4 lg:w-1/2 lg:w-1/3 lg:p-0">
-			<img src="https://images.unsplash.com/photo-1526666923127-b2970f64b422?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60" class="rounded" alt="technology" />
-			<div class="p-4 pl-0">
-				<h2 class="text-2xl font-bold text-gray-800">Put all speaking her delicate recurred possible.</h2>
-				<p class="mt-2 text-gray-700">Set indulgence inquietude discretion insensible bed why announcing. Middleton fat two satisfied additions. So continued he or commanded household smallness delivered. Door poor on do walk in half. Roof his head the what.</p>
-
-				<a href="#" class="ml-auto mt-2 inline-block rounded py-2 text-green-900"> Read more </a>
-			</div>
-		</div>
-
-		<div class="w-full rounded p-4 lg:w-1/2 lg:w-1/3 lg:p-0">
-			<img src="https://images.unsplash.com/photo-1504384764586-bb4cdc1707b0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60" class="rounded" alt="technology" />
-			<div class="p-4 pl-0">
-				<h2 class="text-2xl font-bold text-gray-800">Is at purse tried jokes china ready decay an.</h2>
-				<p class="mt-2 text-gray-700">Small its shy way had woody downs power. To denoting admitted speaking learning my exercise so in. Procured shutters mr it feelings. To or three offer house begin taken am at.</p>
-
-				<a href="#" class="ml-auto mt-2 inline-block rounded py-2 text-green-900"> Read more </a>
-			</div>
-		</div>
-
-		<div class="w-full rounded p-4 lg:w-1/2 lg:w-1/3 lg:p-0">
-			<img src="https://images.unsplash.com/photo-1483058712412-4245e9b90334?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80" class="rounded" alt="technology" />
-			<div class="p-4 pl-0">
-				<h2 class="text-2xl font-bold text-gray-800">As dissuade cheerful overcame so of friendly he indulged unpacked.</h2>
-				<p class="mt-2 text-gray-700">Alteration connection to so as collecting me. Difficult in delivered extensive at direction allowance. Alteration put use diminution can considered sentiments interested discretion.</p>
-
-				<a href="#" class="ml-auto mt-2 inline-block rounded py-2 text-green-900"> Read more </a>
-			</div>
-		</div>
+		<BlogitemCard />
+		<BlogitemCard />
+		<BlogitemCard />
 	</div>
 	<!-- end popular posts -->
 </main>
